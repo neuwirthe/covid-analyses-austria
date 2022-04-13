7f25ae036f1726918231544a7b3eb9831198ad20
Erich Neuwirth <erich.neuwirth@univie.ac.at>
2022-04-13 23:24:58 +0200

    2022-04-13 23:24 MegaMax

--- utils/base_utils.R
+++ utils/base_utils.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE------------------------------
+## ----setup, include=FALSE-----------------------------
 knitr::opts_chunk$set(
   echo = FALSE,
   warning = FALSE,
@@ -6,7 +6,7 @@ knitr::opts_chunk$set(
 )
 
 
-## ------------------------------------------------------
+## -----------------------------------------------------
 suppressPackageStartupMessages({
   library(tidyverse)
   library(fs)
@@ -16,7 +16,7 @@ suppressPackageStartupMessages({
 })
 
 
-## ------------------------------------------------------
+## -----------------------------------------------------
 pop_austria <-
   tribble(
     ~BundeslandID, ~Bundesland_short, ~Bundesland, ~pop,
@@ -34,10 +34,17 @@ pop_austria <-
   )
 
 
-## ----u26-----------------------------------------------
+## ----u26----------------------------------------------
 theme_date_vert <- function(){
   theme_minimal() +
   theme(axis.text.x = element_text(angle=90,size=7,hjust=1,
                                    vjust=0.5))
+}
+
+
+## -----------------------------------------------------
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
 }
 
